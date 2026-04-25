eef34d2c3dee71517f52355dc0a1b823f9219195
vansh565 <sharmavansh969@gmail.com>
2026-04-25 13:24:10 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -736,5 +736,9 @@ if __name__ == "__main__":
    
     
     # Run Flask app with SocketIO
+import os
+
+port = int(os.environ.get("PORT", 10000))
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
